bcc41fea3218b728c90464852195038927b85590
Euan <thecableton@gmail.com>
2018-09-06 16:00:51 +0100

    added Delete button, undefined error

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const textSubmit = document.querySelector('#new-item-form');
     textSubmit.addEventListener('submit', handleFormSubmit);
+
+  const resetButton = document.querySelector('#delete-all');
+    resetButton.addEventListener('click', handleButtonClick);
 })
 
 const handleFormSubmit = function(event){
@@ -11,4 +14,15 @@ const handleFormSubmit = function(event){
   newListItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
   const list = document.querySelector('ul');
   list.appendChild(newListItem);
+  document.getElementById("new-item-form").reset();
 }
+
+const handleButtonClick = function(){
+  const list = document.querySelector('ul');
+  const listItem = document.getElementsByTagName('li');
+  list.removeChild(listItem);
+}
+
+// function removeAll(){
+//   const li = document.getele
+// }
